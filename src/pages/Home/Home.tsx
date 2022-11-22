fb9650f4331af6ec21675b727f9d74b2234b63ba
Julio Macedo <juliocesar1076@gmail.com>
2022-11-22 17:34:20 -0300

    feat: auto redirect

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,13 +1,19 @@
 import * as S from "./styles";
 import GoogleButton from "react-google-button";
 import { useAuth } from "../../context/AuthContext";
+import { Navigate } from "react-router-dom";
 
 export const Home = () => {
   const { googleSignIn, user } = useAuth();
+  console.log(`dados do usuario: ${user?.displayName}`);
 
   return (
     <S.Container>
-      <GoogleButton onClick={() => googleSignIn()} />
+      {user ? (
+        <Navigate replace to="/chat" />
+      ) : (
+        <GoogleButton onClick={() => googleSignIn()} />
+      )}
     </S.Container>
   );
 };
