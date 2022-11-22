621ecd85e076f7b55ae1c6f7a821339616652d48
Julio Macedo <juliocesar1076@gmail.com>
2022-11-22 01:15:25 -0300

    feat: config auth context

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,14 +1,20 @@
-import { useContext, createContext, ReactNode, useEffect, useState } from "react";
+import {
+  useContext,
+  createContext,
+  ReactNode,
+  useEffect,
+  useState,
+} from "react";
 import {
   GoogleAuthProvider,
   signInWithPopup,
   signInWithRedirect,
   signOut,
-  onAuthStateChanged
+  onAuthStateChanged,
+  User,
 } from "firebase/auth";
 
-import { auth } from "../Services/firebase";
-
+import { auth } from "../services/firebase";
 
 interface AuthContextProps {
   children: ReactNode;
@@ -16,51 +22,47 @@ interface AuthContextProps {
 
 interface AuthContextType {
   googleSignIn: () => void;
+  user: User | null;
 }
 
 const AuthContext = createContext({} as AuthContextType);
 
-export const AuthContextProvider = ({children} :AuthContextProps) => {
-  const [user, setUser] = useState({});
+export const AuthContextProvider = ({ children }: AuthContextProps) => {
+  const [user, setUser] = useState<User | null>(null);
 
   const googleSignIn = async () => {
     const provider = new GoogleAuthProvider();
     signInWithPopup(auth, provider)
       .then((result) => {
-
         const user = result.user;
         console.log(user);
-      }).catch((error) => {
-      // Handle Errors here.
+      })
+      .catch((error) => {
+        // Handle Errors here.
         const errorCode = error.code;
         const errorMessage = error.message;
         // The email of the user's account used.
         const email = error.customData.email;
         // The AuthCredential type that was used.
         const credential = GoogleAuthProvider.credentialFromError(error);
-      // ...
+        // ...
       });
   };
 
-  // useEffect(() => {
-  //   const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-  //     setUser(currentUser);
-  //   });
-  //   return () => {
-  //     unsubscribe;
-  //   };
-  // }, []);
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+    return () => {
+      unsubscribe;
+    };
+  }, []);
 
   return (
-    <AuthContext.Provider
-
-      value={{googleSignIn}}
-    >
+    <AuthContext.Provider value={{ googleSignIn, user }}>
       {children}
     </AuthContext.Provider>
   );
 };
 
-
-export const UserAuth = () => useContext(AuthContext);
-
+export const useAuth = () => useContext(AuthContext);
