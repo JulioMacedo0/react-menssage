bbcc2b6628445c761bf376a27cdb009c5d67ff77
Julio Macedo <juliocesar1076@gmail.com>
2022-11-22 01:16:38 -0300

    feat: add defaults colors

--- src/styles/themes/default.ts
+++ src/styles/themes/default.ts
@@ -1,5 +1,11 @@
 export const defaultTheme = {
-
-  "background": "#a2a2a2",
-
+  background: "#36454F",
+  primary: "#007dfe",
+  "primary-light": "#0099ff",
+  "grey-200": "#f8f9fb",
+  white: "#fff",
+  "white-600": "#f8f9fb",
+  "title-name-user": "#434343",
+  "input-chat-background": "#ebebeb",
+  online: "#6ecb44",
 } as const;
