0309e7a1c0f59f161bc4ce3b2d7bba8f82f091a0
Julio Macedo <juliocesar1076@gmail.com>
2022-11-22 01:15:42 -0300

    feat: create chat page

--- src/pages/Chat/Chat.tsx
+++ src/pages/Chat/Chat.tsx
@@ -1,3 +1,103 @@
+import {
+  AddressBook,
+  Archive,
+  ClockCounterClockwise,
+  DotsThreeVertical,
+  NavigationArrow,
+  Paperclip,
+  PlusCircle,
+  Smiley,
+  Users,
+} from "phosphor-react";
+import { Input } from "../../components/Input/Input";
+import { UserChat } from "../../components/UserChat/UserChat";
+import { UserHeader } from "../../components/UserHeader/UserHeader";
+import * as S from "./styles";
+
 export const Chat = () => {
-  return <h1>Chat</h1>;
+  return (
+    <S.Container>
+      <S.Sidebar>
+        <S.Margin>
+          <S.HeaderSidebar>
+            <span>React Chat</span>
+
+            <div>
+              <PlusCircle size={32} weight="fill" />
+              <DotsThreeVertical size={32} weight="fill" />
+            </div>
+          </S.HeaderSidebar>
+
+          <Input />
+
+          <S.NavChat>
+            <ClockCounterClockwise size={26} />
+            <Users size={26} />
+            <AddressBook size={26} />
+            <Archive size={26} />
+          </S.NavChat>
+        </S.Margin>
+        <UserChat
+          image_url="https://images.unsplash.com/photo-1615109398623-88346a601842?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80"
+          name="Richards Hendrics"
+          lastMessage="Sup man, Wanna go out?"
+          unreadMessage={1}
+          selected={false}
+        />
+        <UserChat
+          image_url="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80"
+          name="Elazebeth Nelson"
+          lastMessage="are you sure This isn't ovedoinggg"
+          unreadMessage={3}
+          selected={true}
+        />
+        <UserChat
+          image_url="https://images.unsplash.com/photo-1564564244660-5d73c057f2d2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1176&q=80"
+          name="Jaed Dunn"
+          lastMessage="I can't wait to hear All about the"
+          unreadMessage={0}
+          selected={false}
+        />
+        <UserChat
+          image_url="https://images.unsplash.com/photo-1536552518043-62dcb2723115?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+          name="Gilfoyle"
+          lastMessage="it's, it's nothing... just my imagine"
+          unreadMessage={4}
+          selected={false}
+        />
+        <UserChat
+          image_url="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+          name="Dinesh"
+          lastMessage="Typing..."
+          unreadMessage={0}
+          selected={false}
+        />
+        <UserChat
+          image_url="https://images.unsplash.com/photo-1562124638-724e13052daf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
+          name="Big Head"
+          lastMessage="wish cloud always help out."
+          unreadMessage={0}
+          selected={false}
+        />
+      </S.Sidebar>
+
+      <S.Chat>
+        <UserHeader />
+        <S.Content></S.Content>
+        <S.Footer>
+          <div className="w-input-container">
+            <div className="w-input-text-group">
+              <div id="w-input-text" contentEditable></div>
+              <div className="w-placeholder">Type a message</div>
+            </div>
+          </div>
+          <S.ContainerButtons>
+            <Smiley size={25} />
+            <Paperclip size={25} />
+            <NavigationArrow size={38} />
+          </S.ContainerButtons>
+        </S.Footer>
+      </S.Chat>
+    </S.Container>
+  );
 };
---
+++ src/pages/Chat/styles.ts
@@ -0,0 +1,139 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  max-width: 90rem;
+  height: 800px;
+  margin: 5rem auto;
+`;
+
+export const Sidebar = styled.div`
+  width: 20%;
+  height: 100%;
+  background-color: ${(props) => props.theme.primary};
+  border-radius: 16px;
+`;
+
+export const Margin = styled.div`
+  margin: 0 1.2rem;
+`;
+export const HeaderSidebar = styled.div`
+  display: flex;
+  justify-content: space-between;
+  justify-items: center;
+  width: 100%;
+  height: 1rem;
+  padding: 1.5rem 0;
+  margin-bottom: 1.8rem;
+
+  span {
+    font-size: 1.5rem;
+    color: ${(props) => props.theme["white"]};
+  }
+  > div {
+    display: flex;
+    justify-content: space-between;
+
+    svg {
+      color: ${(props) => props.theme["white"]};
+    }
+  }
+`;
+
+export const NavChat = styled.nav`
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+  display: flex;
+
+  justify-content: space-between;
+
+  svg {
+    color: ${(props) => props.theme["white"]};
+  }
+`;
+
+export const Chat = styled.div`
+  width: 80%;
+  // height: 100%;
+  background-color: ${(props) => props.theme["grey-200"]};
+`;
+
+export const Content = styled.main`
+  height: 80%;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Footer = styled.footer`
+  background-color: ${(props) => props.theme["input-chat-background"]};
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+
+  .w-input-container {
+    width: 80%;
+    height: 4rem;
+    box-sizing: border-box;
+    padding: 9px 18px 11px;
+
+    border: 1px solid ${(props) => props.theme["input-chat-background"]};
+  }
+  .w-input-text-group {
+    position: relative;
+  }
+
+  .w-placeholder {
+    color: #a0a0a0;
+    top: 0;
+    pointer-events: none;
+    user-select: none;
+    position: absolute;
+    opacity: 0;
+    transition: 0.2s padding ease-in-out;
+  }
+
+  #w-input-text {
+    overflow-x: hidden;
+    overflow-y: auto;
+    white-space: pre-wrap;
+    word-wrap: break-word;
+    z-index: 1;
+    max-height: 40px;
+    min-height: 20px;
+    padding: 0 0 0 2px;
+    outline: 0;
+    transition: 0.2s padding ease-in-out;
+  }
+
+  #w-input-text:empty + .w-placeholder {
+    opacity: 1;
+  }
+
+  #w-input-text:focus + .w-placeholder {
+    padding: 0 6px 0px;
+  }
+
+  #w-input-text:focus {
+    padding: 0 6px 0px;
+  }
+`;
+
+export const ContainerButtons = styled.div`
+  gap: 0.8rem;
+  display: flex;
+  align-items: center;
+  padding: 0 1rem;
+  svg {
+    color: #a0a0a0;
+  }
+
+  > :last-child {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 100%;
+    padding: 8px;
+    color: white;
+    background-color: ${(props) => props.theme.primary};
+  }
+`;
