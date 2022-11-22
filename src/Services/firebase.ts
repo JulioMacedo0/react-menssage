171e904df3a8d80e0de7a1fdb3a19f4e8ec1ce27
Julio Macedo <juliocesar1076@gmail.com>
2022-11-22 01:16:11 -0300

    feat: firebase configs

--- src/Services/firebase.ts
+++ src/Services/firebase.ts
@@ -1,7 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-
 const firebaseConfig = {
   apiKey: "AIzaSyC2X4JeQeI5iz6jHBfNqc8N_gORZoRVGuY",
   authDomain: "react-menssage-fe2aa.firebaseapp.com",
@@ -9,19 +8,11 @@ const firebaseConfig = {
   storageBucket: "react-menssage-fe2aa.appspot.com",
   messagingSenderId: "1070982287327",
   appId: "1:1070982287327:web:dcc7de2087037e0ae3be6a",
-  measurementId: "G-0NWEQ1MYM0"
+  measurementId: "G-0NWEQ1MYM0",
 };
-
-
 
 const firebase = initializeApp(firebaseConfig);
 const auth = getAuth(firebase);
 auth.languageCode = "it";
 
-
-
-
-
-
-
-export {  auth };
+export { auth };
