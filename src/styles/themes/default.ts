decd18abb990d9e839f7f824399cc6c98a51355b
Julio Macedo <juliocesar1076@gmail.com>
2022-11-24 12:43:39 -0300

    feat: add primary-hover color

--- src/styles/themes/default.ts
+++ src/styles/themes/default.ts
@@ -1,13 +1,18 @@
 export const defaultTheme = {
   background: "#36454F",
-  primary: "#007dfe",
+
+  primary: "#006afff7",
+  "primary-hover": " #007dfe",
   "primary-light": "#0099ff",
+
+  grey: "#868686",
   "grey-200": "#f8f9fb",
+
   white: "#fff",
   "white-600": "#f8f9fb",
+
   "title-name-user": "#434343",
   "input-chat-background": "#ebebeb",
 
-  grey: "#868686",
   online: "#6ecb44",
 } as const;
