c87644270f6dc37ae906bf42afd263da2c82fd6f
Julio Macedo <juliocesar1076@gmail.com>
2022-11-24 12:39:04 -0300

    feat: add button and style button

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -2,18 +2,40 @@ import styled from "styled-components";
 
 export const Container = styled.form`
   display: flex;
+  width: 100%;
+  height: 2.5rem;
+  align-items: center;
+  justify-content: space-between;
+  border-radius: 8px;
+
+  background-color: ${(props) => props.theme["white"]};
+  button {
+    background-color: ${(props) => props.theme["primary-light"]};
+    color: ${(props) => props.theme["white"]};
+
+    border: none;
+    height: 100%;
+  }
+
+  > svg {
+    color: ${(props) => props.theme["primary"]};
+    cursor: default;
+    margin-left: 0.5rem;
+  }
 `;
 
 export const Input = styled.input`
+  outline: none;
   border: none;
   width: 100%;
-  height: 2.5rem;
+  height: 100%;
   padding: 1rem;
+  background-color: transparent;
 
   ::placeholder {
-    color: ${(props) => props.theme["white"]};
+    color: ${(props) => props.theme["primary"]};
+    font-size: 0.9rem;
   }
 
-  background-color: ${(props) => props.theme["primary-light"]};
-  color: ${(props) => props.theme["white"]};
+  color: ${(props) => props.theme["primary"]};
 `;
