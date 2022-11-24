6dfd2e2bfccd56a6812f2ea8df32b119d9f18ec0
Julio Macedo <juliocesar1076@gmail.com>
2022-11-24 12:38:25 -0300

    feat: chart context

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import { AuthContextProvider } from "./context/AuthContext";
+import { ChatContextProvider } from "./context/ChatContext";
 import { Router } from "./Router";
 import { GlobalStyle } from "./styles/global";
 
@@ -10,10 +11,12 @@ export const App = () => {
   return (
     <ThemeProvider theme={defaultTheme}>
       <AuthContextProvider>
-        <GlobalStyle />
-        <BrowserRouter>
-          <Router />
-        </BrowserRouter>
+        <ChatContextProvider>
+          <GlobalStyle />
+          <BrowserRouter>
+            <Router />
+          </BrowserRouter>
+        </ChatContextProvider>
       </AuthContextProvider>
     </ThemeProvider>
   );
