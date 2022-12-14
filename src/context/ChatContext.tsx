e58e9843237b5f3679c2823cd9aa0490dfe218f1
Julio Macedo <juliocesar1076@gmail.com>
2022-12-13 22:49:27 -0300

    feat: demove function

--- src/context/ChatContext.tsx
+++ src/context/ChatContext.tsx
@@ -167,7 +167,7 @@ export const ChatContextProvider = ({ children }: ChatContextProps) => {
   const updateChat = async () => {
     await getChat();
     await  updateCurrentChat();
-    await scrollToBottom();
+  //  await scrollToBottom();
   };
 
   useEffect(  () => {
