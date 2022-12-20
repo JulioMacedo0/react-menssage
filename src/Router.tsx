34380ed83ee27ab15bed07e0d419b4e8a9989fb8
Julio Macedo <juliocesar1076@gmail.com>
2022-12-20 14:54:58 -0300

    feat: protected route

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,14 +1,17 @@
 import { Routes, Route } from "react-router-dom";
+import { useAuth } from "./context/AuthContext";
 import { Chat } from "./pages/Chat/Chat";
 import { Home } from "./pages/Home/Home";
 import { NotFound } from "./pages/NotFound/NotFound";
 
 export function Router() {
+
+  const {  user } = useAuth();
   return (
     <Routes>
       <Route path="*" element={<NotFound />} />
       <Route path="/" element={<Home />} />
-      <Route path="/chat" element={<Chat />} />
+      <Route path="/chat" element={ user ? <Chat /> : <Home />} />
     </Routes>
   );
 }
