c37ec96aad7a303bdf3d2540266dcc59a1c846ab
Julio Macedo <juliocesar1076@gmail.com>
2022-12-21 09:13:03 -0300

    feat: pulse effect in online span

--- src/components/UserHeader/styles.ts
+++ src/components/UserHeader/styles.ts
@@ -10,6 +10,23 @@ export const Container = styled.div`
   padding: 2rem 1.5rem;
   box-shadow: 0px 2px 2px 1px rgba(0, 0, 0, 0.2);
 
+  @keyframes pulse {
+	0% {
+		transform: scale(0.95);
+		box-shadow: 0 0 0 0 rgba(0, 255, 0, 1);
+	}
+
+	70% {
+		transform: scale(1);
+		box-shadow: 0 0 0 10px rgba(0, 0, 0, 0);
+	}
+
+	100% {
+		transform: scale(0.95);
+		box-shadow: 0 0 0 0 rgba(0, 0, 0, 0);
+	}
+}
+
   p {
     font-size: 1.5rem;
     font-weight: bold;
@@ -26,14 +43,33 @@ export const Container = styled.div`
       background-color: ${(props) => props.theme.primary};
       padding:0.5rem;
       border-radius: 50%;
+      transition: transform .5s ease-in-out;
+
+       &:hover {
+      cursor: pointer;
+      transform: rotate(360deg);
+      background-color: ${(props) => props.theme["primary-hover"]};
+    }
     }
 
     span {
-      height: 0.5rem;
-      width: 0.5rem;
+      height: 0.65rem;
+      width: 0.65rem;
       background-color: ${(props) => props.theme.online};
       border-radius: 50%;
       display: inline-block;
+
+      box-shadow: 0 0 0 0 rgba(0, 0, 0, 1);
+	    transform: scale(1);
+	    animation: pulse 2s infinite;
+    }
+  }
+
+  .nameDiv{
+
+    flex: 0.97;
+    :hover{
+      cursor: pointer;
     }
   }
 `;
